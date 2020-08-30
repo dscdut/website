dc46891b83997893b2a571e9fb0264965201a6fa
Bittermeatball <nguyenhuunguyeny.ny@gmail.com>
2020-08-30 10:06:47 +0700

    :art: :hammer: Add vuex constants and theming colors

---
+++ constants/vuex/index.js
@@ -0,0 +1,5 @@
+export { default as rootActions } from './root/actions'
+export { default as rootMutations } from './root/mutations'
+
+export { default as authActions } from './auth/actions'
+export { default as authMutations } from './auth/mutations'
