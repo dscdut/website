8762a27b8a8cf7264f0b3880d3fc98cdfec94d6f
Bittermeatball <nguyenhuunguyeny.ny@gmail.com>
2020-10-25 10:52:41 +0700

    Fix ssr problem with v-clip

--- client/utils/directives/modules/clip.js
+++ client/utils/directives/modules/clip.js
@@ -1,5 +1,5 @@
 // This or simply use clipboard npm package, but vanilla it is :D
-const copyToClipboard = (function initClipboardText() {
+function copyToClipboard(text) {
   const textarea = document.createElement('textarea')
   // Move it off-screen.
   textarea.style.cssText = 'position: absolute; left: -99999em'
@@ -7,42 +7,40 @@ const copyToClipboard = (function initClipboardText() {
   // text is .select()'ed.
   textarea.setAttribute('readonly', true)
   document.body.appendChild(textarea)
-  return function setClipboardText(text) {
-    textarea.value = text
-    // Check if there is any content selected previously.
-    const selected =
-      document.getSelection().rangeCount > 0
-        ? document.getSelection().getRangeAt(0)
-        : false
-    // iOS Safari blocks programmatic execCommand copying normally, without this hack.
-    // https://stackoverflow.com/questions/34045777/copy-to-clipboard-using-javascript-in-ios
-    if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
-      const editable = textarea.contentEditable
-      textarea.contentEditable = true
-      const range = document.createRange()
-      range.selectNodeContents(textarea)
-      const sel = window.getSelection()
-      sel.removeAllRanges()
-      sel.addRange(range)
-      textarea.setSelectionRange(0, 999999)
-      textarea.contentEditable = editable
-    } else {
-      textarea.select()
-    }
-    try {
-      const result = document.execCommand('copy')
-      // Restore previous selection.
-      if (selected) {
-        document.getSelection().removeAllRanges()
-        document.getSelection().addRange(selected)
-      }
-      return result
-    } catch (err) {
-      console.error(err)
-      return false
-    }
+  textarea.value = text
+  // Check if there is any content selected previously.
+  const selected =
+    document.getSelection().rangeCount > 0
+      ? document.getSelection().getRangeAt(0)
+      : false
+  // iOS Safari blocks programmatic execCommand copying normally, without this hack.
+  // https://stackoverflow.com/questions/34045777/copy-to-clipboard-using-javascript-in-ios
+  if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+    const editable = textarea.contentEditable
+    textarea.contentEditable = true
+    const range = document.createRange()
+    range.selectNodeContents(textarea)
+    const sel = window.getSelection()
+    sel.removeAllRanges()
+    sel.addRange(range)
+    textarea.setSelectionRange(0, 999999)
+    textarea.contentEditable = editable
+  } else {
+    textarea.select()
   }
-})()
+  try {
+    const result = document.execCommand('copy')
+    // Restore previous selection.
+    if (selected) {
+      document.getSelection().removeAllRanges()
+      document.getSelection().addRange(selected)
+    }
+    return result
+  } catch (err) {
+    console.error(err)
+    return false
+  }
+}
 export default {
   bind(el, binding, vnode, oldVnode) {
     if (binding.value.length > 0) {
